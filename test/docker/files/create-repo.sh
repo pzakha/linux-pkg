871bfdcea1a5c46e1f3108dba69ee9183eec3460
Pavel Zakharov <pavel.zakharov@delphix.com>
2018-12-28 13:56:33 -0500

    use alpine linux docker image

---
+++ test/docker/files/create-repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+repo="$1"
+dir="/srv/git/${repo}.git"
+
+rm -rf "$dir"
+mkdir "$dir"
+cd "$dir"
+git init --bare
+git config --local http.receivepack true
+git update-server-info
+chown -R nginx:nginx "$dir"
+chmod -R 754 "$dir"
