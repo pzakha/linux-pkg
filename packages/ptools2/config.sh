d6cb543d44604711a66a465847bd78760b06c6ac
Pavel Zakharov <pavel.zakharov@delphix.com>
2020-09-08 17:26:22 -0400

    add ptools2 package

---
+++ packages/ptools2/config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright 2018, 2019 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/pzakha/ptools.git"
+
+function prepare() {
+	logmust install_pkgs cargo devscripts
+}
+
+function build() {
+	logmust cd "$WORKDIR/repo"
+	PACKAGE_VERSION=$(sed -rn 's/version = "(.*)"/\1/p' <Cargo.toml)
+
+	logmust dpkg_buildpackage_default
+}
