666f570f9db6ec4b96be55ce7331e95b0bb034e2
Pavel Zakharov <pavel.zakharov@delphix.com>
2020-11-16 17:20:26 -0500

    TEST: use pzakha's delphix-platform

--- packages/delphix-platform/config.sh
+++ packages/delphix-platform/config.sh
@@ -16,7 +16,7 @@
 #
 # shellcheck disable=SC2034
 
-DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/delphix-platform.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/pzakha/delphix-platform.git"
 DEFAULT_PACKAGE_VERSION="1.0.0"
 
 function build() {
